b096096269122735f4e5b688264585b0ce9a900e
hoserdude <tim@hoserdude.com>
2015-03-27 21:57:49 -0700

    upgrade to 1.2.0.RELEASE, add Spring repository

--- src/main/java/com/hoserdude/samples/boot/config/SuchApplication.java
+++ src/main/java/com/hoserdude/samples/boot/config/SuchApplication.java
@@ -3,13 +3,9 @@ package com.hoserdude.samples.boot.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 public class SuchApplication implements CommandLineRunner {
 
 	@Autowired
